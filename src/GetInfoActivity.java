704583b545b8b22ceeb9442f5a89067ba59f3d4f
LizCira <liz.cira.goldstein@gmail.com>
2014-08-21 02:52:59 -0400

    added button to layout and pseudo java IP script from stack overflow

---
+++ src/GetInfoActivity.java
@@ -0,0 +1,47 @@
+package com.GetInfo;
+
+import java.net.InetAddress;
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MyAndroidAppActivity extends Activity {
+
+
+//fount script on: http://stackoverflow.com/questions/11777676/how-can-i-get-my-dynamic-ip-address-of-my-android-device
+
+  Button button;
+
+  @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button b=(Button)findViewById(R.id.btn1);
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+                 public static String getLocalIpAddressString() {
+            try {
+                for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                    NetworkInterface intf = en.nextElement();
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()) {
+                            return inetAddress.getHostAddress().toString();
+                        }
+                    }
+                }
+            }catch (SocketException ex) {
+            }
+
+            return null;
+        }
+        });
+    }
+
+  }
